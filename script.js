025933ca8920946dd40d5e6e5eea78b60478e110
cwsprandel <110702891+cwsprandel@users.noreply.github.com>
2023-04-05 20:44:54 -0500

    Comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let gameSpaces = [];
 
+//determine which player is going first (whoever selects X)
+//pass emblem into changeStatus parameter 
+//change to new player and emblem
 
-
-function changeStatus(id, emblem) {
+function changeStatus(id) {
     let emblem = "O";
     document.getElementById(id).textContent = emblem;
 }
